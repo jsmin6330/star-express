a28defe95949900fbdab81e969d8b1d0c959f82f
jsmin6330 <jsmin6330@gmail.com>
2024-02-05 15:50:56 +0900

    Route_parameters

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// Route parameters
 
-app.get('/', (req, res) => {
-	//res.send('Hello World!')
-	let myJson = { "Hello": "World"}
-	res.json(myJson)
-})
+const express = require('express');
+const router = express.Router();
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+router.get('/artists/:id', function(req, res){
+	console.log("id는 " + req.params.id + " 입니다")
+	res.send("id: " + req.params.id)
+});
+
+
+//여러개도 가능
+router.get('/artists/:id/company/:company', function(req, res){
+	console.log("id는 " + req.params.id + " 회사: " + req.params.company)
+});
